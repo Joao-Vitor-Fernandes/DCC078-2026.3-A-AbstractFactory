d547c89cd82ad33992c733f7c74ddb0b0c5aea5a
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2026-09-20 21:43:18 -0300

    adicionar pedido para etiqueta e nota fiscal

---
+++ src/main/java/padroescriacao/abstractfactory/Pedido.java
@@ -0,0 +1,20 @@
+package padroescriacao.abstractfactory;
+
+public class Pedido {
+
+    private Etiqueta etiqueta;
+    private NotaFiscal notaFiscal;
+
+    public Pedido(FabricaLogistica fabrica) {
+        this.etiqueta = fabrica.createEtiqueta();
+        this.notaFiscal = fabrica.createNotaFiscal();
+    }
+
+    public String emitirEtiqueta() {
+        return this.etiqueta.emitir();
+    }
+
+    public String emitirDocumentoFiscal() {
+        return this.notaFiscal.emitir();
+    }
+}
