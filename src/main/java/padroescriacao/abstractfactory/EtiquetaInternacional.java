b1502eba3b6395512169a76790d5f2f92829dca3
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2026-09-20 21:16:16 -0300

    criar etiquetas nacional e internacional para interface etiqueta

---
+++ src/main/java/padroescriacao/abstractfactory/EtiquetaInternacional.java
@@ -0,0 +1,8 @@
+package padroescriacao.abstractfactory;
+
+public class EtiquetaInternacional implements Etiqueta {
+
+    public String emitir() {
+        return "Etiqueta de Envio Internacional (Exportação)";
+    }
+}
---
+++ src/main/java/padroescriacao/abstractfactory/EtiquetaNacional.java
@@ -0,0 +1,8 @@
+package padroescriacao.abstractfactory;
+
+public class EtiquetaNacional implements Etiqueta {
+
+    public String emitir() {
+        return "Etiqueta de Envio Nacional (Correios/Transportadora)";
+    }
+}
